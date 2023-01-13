d75fac0ed0e75fec336af91b70d2755d4ecd53ce
hllvc <hllvc@icloud.com>
2023-01-13 15:59:23 +0100

    base64 decode providers creds

--- provider.tf
+++ provider.tf
@@ -2,10 +2,10 @@
 provider "kubernetes" {
   host = var.cluster_endpoint
 
-  client_certificate     = var.client_certificate
-  client_key             = var.client_key
-  cluster_ca_certificate = var.cluster_ca_certificate
-  # client_certificate     = base64decode(var.client_certificate)
-  # client_key             = base64decode(var.client_key)
-  # cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+  # client_certificate     = var.client_certificate
+  # client_key             = var.client_key
+  # cluster_ca_certificate = var.cluster_ca_certificate
+  client_certificate     = base64decode(var.client_certificate)
+  client_key             = base64decode(var.client_key)
+  cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
 }
